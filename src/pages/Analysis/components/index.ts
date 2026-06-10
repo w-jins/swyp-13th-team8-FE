c56efd9d1923e3309154e5fd2216abd3d3ff40ad
w-jins <jinneen@naver.com>
2026-06-10 17:13:54 +0900

    feat: 모듈 패킹 작업

---
+++ src/pages/Analysis/components/index.ts
@@ -0,0 +1,10 @@
+// ============ RESULT DOMAIN ==============
+export { default as AnalysisResultMobileTop } from './result/AnalysisResultMobileTop';
+export { default as AnalysisResultCategories } from './result/AnalysisResultCategories';
+export { default as ResultSummarySection } from './result/ResultSummarySection';
+export { default as PdfViewer } from './result/PdfViewer';
+
+// ============ UPLOAD DOMAIN ==============
+export { default as UploadSection } from './upload/UploadSection';
+export { default as HistorySection } from './upload/HistorySection';
+export { default as AnalysisTabs } from './upload/AnalysisTabs';
